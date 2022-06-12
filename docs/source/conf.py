3c19d1d3e52a51dac55395d857bcc90c2cd5cf0d
Benjamin Weymouth <benjamin.weymouth@gmail.com>
2022-06-12 19:21:41 -0400

    theme change v2
    theme change v2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
+html_theme_path = ["_themes", ]
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
